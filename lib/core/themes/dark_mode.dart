db5b7dad982ab35138b162572dacd1206f40b51f
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-05-26 02:50:38 +0200

    git

---
+++ lib/core/themes/dark_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkmode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    surface: Colors.grey.shade300,
+    primary: Colors.grey.shade600,
+    secondary: Colors.grey.shade800,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade300,
+  ),
+);
