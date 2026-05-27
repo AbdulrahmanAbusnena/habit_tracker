fe1204973f566c99867fa887e9a7c989480dba70
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-05-28 00:12:05 +0200

    we are done with converting the habit class into a map

--- lib/domain/models/habit.dart
+++ lib/domain/models/habit.dart
@@ -14,4 +14,20 @@ class Habit {
     required this.createdAt,
     List<String>? completedDays,
   }) : completedDays = completedDays ?? [];
+
+  // DB conversion
+  // SQLite can't store a list (duh uh) so the approah I found was joining it into one string
+
+  Map<String, dynamic> toMap() => {
+    'id': id,
+    'name': name,
+    'description': description,
+    'colorIndex': colorIndex,
+    /* toIso8601String() is a format structure that strictly follows yyyy-MM-ddThHH:mm 
+   or whatever and it's database friendtly, because we can't use Dart's Dart and TIme 
+   */
+    'createdAt': createdAt.toIso8601String(),
+    // .join(), method creates a single string by placing a specfic connector between items (I'm using it to connect the dates in the list)
+    'completedDays': completedDays.join(','),
+  };
 }
