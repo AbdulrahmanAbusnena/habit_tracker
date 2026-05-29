a9461aab117d5054a49dc329b8cca24551f5d53c
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-05-29 20:21:19 +0200

    just created the /shared folder and added the home_page drawer

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/core/themes/theme_provider.dart';
+import 'package:habit_tracker/shared/drawer.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,18 +16,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        child: Center(
-          child: CupertinoSwitch(
-            value: Provider.of<ThemeProvider>(context).isDarkMode,
-            onChanged: (value) => Provider.of<ThemeProvider>(
-              context,
-              listen: false,
-            ).toggleTheme(),
-          ),
-        ),
-      ),
+      drawer: HomeDrawer(),
       // body: Column(children: [Text('nigga boy')]),
     );
   } // told you
