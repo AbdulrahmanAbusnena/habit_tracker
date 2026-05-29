3c6e0ccd0ddc07a8586b44bda542ed05320dd80e
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-05-29 19:42:06 +0200

    done with testing and running the instances on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/core/themes/theme_provider.dart';
+import 'package:habit_tracker/domain/providers/habit_provider.dart';
 import 'package:habit_tracker/screens/home_page.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(
+          create: (context) => HabitProvider()..loadHabits(),
+        ),
+      ],
       child: const MyApp(),
     ),
   );
